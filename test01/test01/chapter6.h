2a1693736e18827ecbdec335a623e3c807ff409b
bailijiang <bailijiang@hotmail.com>
2017-12-11 11:32:50 +0800

    chapter06 function  finished…

--- test01/test01/chapter6.h
+++ test01/test01/chapter6.h
@@ -8,4 +8,18 @@
 
 #include <iostream>
 
+using namespace std;
+using ArrT = string[3];
+
 size_t count_calls();
+void swap(int *pt1, int *pt2);
+void reset(int &i);
+bool findUpper(string const &str);
+void displayArr(const int *, const int *);
+string (&reArr(string (&arr)[3]))[3];
+ArrT &func1(ArrT &arr);
+auto func2(ArrT &arr) -> string (&)[3];
+
+ArrT &func3(ArrT &arr);
+
+int f(int a, int b);
--- test01/test01/fact.cc
+++ test01/test01/fact.cc
@@ -1,7 +1,41 @@
 #include "chapter6.h"
-#include <iostream>
 
 size_t count_calls() {
   static size_t count = 0;
   return ++count;
 }
+
+void swap(int *pt1, int *pt2) {
+  int temp;
+  temp = *pt1;
+  *pt1 = *pt2;
+  *pt2 = temp;
+}
+
+void reset(int &i) { i = 0; }
+
+bool findUpper(string const &str) {
+  for (auto ch : str) {
+    if (isupper(ch))
+      return true;
+  }
+  return false;
+}
+
+void displayArr(const int *begin, const int *end) {
+  while (begin != end) {
+    cout << *begin++ << endl;
+  }
+}
+
+string (&reArr(string (&arr)[3]))[3] { return arr; }
+
+ArrT &func1(ArrT &arr) { return arr; }
+
+auto func2(ArrT &arr) -> string (&)[3] { return arr; }
+
+string arrS[3];
+
+decltype(arrS) &func3(ArrT &arr) { return arr; }
+
+int f(int a, int b) { return a + b; }
